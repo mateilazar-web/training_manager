9768997849b2c4efe5fe287dc0cc6804f7479208
Matei Lazar <matei.lazar@moduscreate.com>
2025-05-08 22:42:04 +0300

    Update to Laravel 10

--- config/app.php
+++ config/app.php
@@ -176,9 +176,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
-        Krlove\EloquentModelGenerator\Provider\GeneratorServiceProvider::class,
-
     ],
 
     /*
