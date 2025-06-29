9f628d735633d4158c4c64ab757e67f657fa80b9
Matei Lazar <matei.lazar@moduscreate.com>
2025-06-29 19:46:57 +0300

    Do not expand team, in request access page

--- resources/views/teams/item.blade.php
+++ resources/views/teams/item.blade.php
@@ -1,10 +1,4 @@
 <td>
-    <a data-bs-toggle="collapse" href="#collapse{{ $team->id }}" role="button" aria-expanded="false" aria-controls="collapseExample">
-        {{ $team->name }}
-    </a>
-    <div class="collapse" id="collapse{{ $team->id }}">
-        <div class="card card-body">
-        </div>
-    </div>
+    {{ $team->name }}
 </td>
 <td class="nowrap">{{ $team->user->name }}</td>
